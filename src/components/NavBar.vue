f4fbae93319119805aef72eb11373127704e802a
SethJKallis <kallisseth@gmail.com>
2023-01-25 15:12:08 +0200

    minor style changes for nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav d-flex justify-content-between w-50 mx-auto">
+      <ul class="navbar-nav d-flex justify-content-around w-50 mx-auto">
         <li class="nav-item">
             <router-link to="/">Home</router-link>
         </li>
@@ -17,9 +17,9 @@
         <li class="nav-item">
             <router-link to="/about">About</router-link>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
             <router-link to="/contact">Contact</router-link>
-        </li>
+        </li> -->
       </ul>
     </div>
 
